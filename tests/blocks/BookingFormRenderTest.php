8e613461810e4f3df2fcc3173837e22cad03214b
TheCollinsByte <collo@fastmail.com>
2026-07-01 14:04:20 +0300

    feat(booking): form fields matching the real API (departure/first/last/pax) (v1.1 task 2)

--- tests/blocks/BookingFormRenderTest.php
+++ tests/blocks/BookingFormRenderTest.php
@@ -20,8 +20,13 @@ class BookingFormRenderTest extends TestCase {
     public function test_renders_booking_form_with_fields(): void {
         $html = kwt_render_booking_form( array(), '' );
         $this->assertStringContainsString( 'id="kwt-book"', $html );
+        $this->assertStringContainsString( 'name="firstName"', $html );
+        $this->assertStringContainsString( 'name="lastName"', $html );
         $this->assertStringContainsString( 'name="email"', $html );
         $this->assertStringContainsString( 'name="phone"', $html );
+        $this->assertStringContainsString( 'name="adults"', $html );
+        $this->assertStringContainsString( 'kwt-booking__departure', $html );
+        $this->assertStringContainsString( 'kwt-booking__price', $html );
         $this->assertStringContainsString( 'kwt-booking__status', $html );
     }
 }

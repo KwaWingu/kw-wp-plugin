38aeff55239a263a1ec26597b1e242920b3a271b
TheCollinsByte <collo@fastmail.com>
2026-07-01 16:13:58 +0300

    build(editor): @wordpress/scripts tooling + Tours Grid edit.js exemplar (ServerSideRender + InspectorControls); commit built bundles; phpcs excludes build/node_modules (v1.3 foundation)

---
+++ build/tours-grid/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'de1e5353c8f99fdf8fc9');
